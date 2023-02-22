c481d322302d916e3bbf1cf4b25fdd00a758afc2
UNIVERSE\aillarionov <illarionoff.a@gmail.com>
2023-02-22 21:52:20 +0100

    Add Models and Data

---
+++ WebApplication1/WebApplication1/Models/ListingRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models;
+
+public class ListingRequest
+{
+    public int Id { get; set; }
+    public string? RequestBody { get; set; }
+    public string? EstimatedCompletionTime { get; set; }
+    public string? RequestStatus { get; set; }
+    public string RequestId { get; set; } = Guid.NewGuid().ToString();
+}
--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=RequestDB.db"));
 var app = builder.Build();
 app.UseHttpsRedirection();
 app.Run();
