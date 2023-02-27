5ac1eada86cab159f35826bf36449df724d1f41f
UNIVERSE\aillarionov <illarionoff.a@gmail.com>
2023-02-27 21:05:18 +0100

    Add Status Endpoint

---
+++ WebApplication1/WebApplication1/Dtos/ListingStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Dtos;
+
+public class ListingStatus
+{
+    public string? RequestStatus { get; set; }
+    public string? EstimatedCompletionTime { get; set; }
+    public string? ResourseUrl { get; set; }
+}
--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
+using WebApplication1.Dtos;
 using WebApplication1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +25,30 @@ app.MapPost("api/v1/products", async (AppDbContext context, ListingRequest reque
     await context.SaveChangesAsync();
 
     return Results.Accepted($"api/v/productstatus/{request.RequestId}", request);
+});
+
+// Status Endpoint
+app.MapGet("api/v1/productstatus/{requestId}", (AppDbContext context, string requestId) =>
+{
+    var listingRequest = context.ListingRequests.FirstOrDefault(x => x.RequestId == requestId);
+    if(listingRequest == null)
+    {
+        return Results.NotFound();
+    }
+
+    ListingStatus listingStatus = new ListingStatus();
+    listingStatus.RequestStatus = listingRequest.RequestStatus;
+    listingStatus.ResourseUrl = string.Empty;
+
+    if(listingRequest.RequestStatus!.ToUpper() == "COMPLETE")
+    {
+        listingStatus.ResourseUrl = $"api/v1/products/{Guid.NewGuid().ToString()}";
+        return Results.Ok(listingStatus);
+    }
+
+    listingStatus.EstimatedCompletionTime = "2023-02-27:21:00:00";
+
+    return Results.Ok(listingStatus);
 });
 
 app.Run();
