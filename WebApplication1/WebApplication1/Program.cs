4f76031c229804b28c11afc79b6ecf68d30f79a2
UNIVERSE\aillarionov <illarionoff.a@gmail.com>
2023-02-27 20:46:04 +0100

    Add Start Endpoint

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,8 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Data;
+using WebApplication1.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite("Data Source=RequestDB.db"));
 var app = builder.Build();
 app.UseHttpsRedirection();
+
+// Start Endpoint
+app.MapPost("api/v1/proucts", async (AppDbContext context, ListingRequest request) =>
+{
+    if(request == null)
+    {
+        return Results.BadRequest();
+    }
+
+    request.RequestStatus = "ACCEPT";
+    request.EstimatedCompletionTime = "2023-02-27:20:45:00";
+
+    await context.ListingRequests.AddAsync(request);
+
+    await context.SaveChangesAsync();
+
+    return Results.Accepted($"api/v/productstatus/{request.RequestId}", request);
+});
+
 app.Run();
